246f1cf32a15bae6e56f934ed2f54651649d9e85
Muhammad Najmuddin Farid <farid.najmuddins@gmail.com>
2022-12-15 18:13:39 +0300

    category and author panel

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,98 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AdminBlog.Models;
+
+namespace AdminBlog.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+    private readonly BlogContext _context;//veri tabani context sinifi tanitma
+
+    public HomeController(ILogger<HomeController> logger, BlogContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+
+    public async Task<IActionResult> AddCategory(Category category){ //Category Nesnesi
+        //Update icin add ile ayni form ile kullaniyorum
+        if(category.Id == 0){//eger daha oncesinde veritabaninda bir kayit yoksa add
+            await _context.AddAsync(category);
+        }else{//var ise update
+            _context.Update(category);
+        }
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Category));
+    }
+
+    public async Task<IActionResult> AddAuthor(Author author){ //Category Nesnesi
+        //Update icin add ile ayni form ile kullaniyorum
+        if(author.Id == 0){//eger daha oncesinde veritabaninda bir kayit yoksa add
+            await _context.AddAsync(author);
+        }else{//var ise update
+            _context.Update(author);
+        }
+        await _context.SaveChangesAsync();
+
+        return RedirectToAction(nameof(Author));
+    }
+
+    public async Task<IActionResult> CategoryDetails(int Id)
+    {
+        var category = await _context.Category.FindAsync(Id);
+        return Json(category);//Category modeli JSON olarak return ediyoruz
+    }
+
+    public async Task<IActionResult> AuthorDetails(int Id)
+    {
+        var author = await _context.Author.FindAsync(Id);
+        return Json(author);//Author modeli JSON olarak return ediyoruz
+    }
+
+    public IActionResult Category()//Add category to list and presented in a table
+    {
+        List<Category> list = _context.Category.ToList();
+        return View(list);
+    }
+
+    public IActionResult Author()//Add category to list and presented in a table
+    {
+        List<Author> list = _context.Author.ToList();
+        return View(list);
+    }
+
+    public async Task<IActionResult> DeleteCategory(int? Id)
+    {
+        Category category = await _context.Category.FindAsync(Id);
+        _context.Remove(category);
+        await _context.SaveChangesAsync();
+        
+        return RedirectToAction(nameof(Category));
+    }
+
+        public async Task<IActionResult> DeleteAuthor(int? Id)
+    {
+        var author = await _context.Author.FindAsync(Id);
+        _context.Remove(author);
+        await _context.SaveChangesAsync();
+        
+        return RedirectToAction(nameof(Author));
+    }
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
